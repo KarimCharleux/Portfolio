9c488fe70e9e5bb8445695f75e0569f525b35603
KarimCharleux <karim.charleux@etu.univ-cotedazur.fr>
2023-04-28 15:12:10 +0200

    Fix responsive header of "Le Monde du VTT"

--- src/app/projet/le-monde-du-vtt/vtt-header/vtt-header.component.ts
+++ src/app/projet/le-monde-du-vtt/vtt-header/vtt-header.component.ts
@@ -11,6 +11,16 @@ export class VttHeaderComponent {
     const menu = document.querySelector('nav');
     if (menu === null) return;
     menu.classList.toggle('show');
+
+    const hamburger = document.querySelector('#header-icon-list');
+    if (hamburger === null) return;
+    hamburger.classList.toggle('active');
+
+    const meats = document.querySelectorAll('.meat');
+    if (meats === null) return;
+    meats.forEach(function (meat) {
+      meat.classList.toggle('active');
+    });
   }
 
   disableMenu() {
